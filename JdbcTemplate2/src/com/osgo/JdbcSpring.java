810253b72b3f23b772ac8a31eefe6dd913b6c479
wade <wade@Phenom>
2013-05-06 13:10:27 +0100

    JdbcTemplate2

---
+++ JdbcTemplate2/src/com/osgo/JdbcSpring.java
@@ -0,0 +1,24 @@
+package com.osgo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.event.ApplicationContextEvent;
+
+import com.osgo.dao.JdbcDaoImpl;
+import com.osgo.model.Circle;
+
+public class JdbcSpring
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		JdbcDaoImpl dao = ctx.getBean("jdbcDaoImpl", JdbcDaoImpl.class);
+				
+		// Circle circle = dao.getCircle(1);
+		//System.out.println(circle.getName());
+		
+		System.out.println(dao.getCircleName(1));
+
+	} // end method main
+
+} // end Class jdbcDemo
